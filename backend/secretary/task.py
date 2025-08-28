90c0a26facd32a800c9da6eccdbbcef2dfcb466b
Honorh23 <131210863+Honorh23@users.noreply.github.com>
2025-08-28 18:07:56 +0200

    portfolio frontend

--- backend/secretary/task.py
+++
@@ -1,4 +0,0 @@
-from .utils import send_daily_itinerary
-
-def daily_itinerary_task():
-    send_daily_itinerary()
--- backend/secretary/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SecretaryChatView, GuestMessageView, GuestMessageListView
-
-urlpatterns = [
-    path("chat/", SecretaryChatView.as_view(), name="secretary-chat"),
-    path("guest-message/", GuestMessageView.as_view(), name="guest_message"),
-    path("guest-messages/", GuestMessageListView.as_view(), name="guest_messages_list"),
-]
